1ce2ea9c58d826c9c50c2d4966bce25057c0846b
Jerome Bernard <jerome.bernard@kalixia.com>
2016-07-08 10:46:27 +0200

    Initial working container. Missing Redis cache support yet.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # docker-jeedom-nginx
 Docker image for Jeedom on Nginx
+
+This is heavily based on the official Jeedom installation documentation with Nginx.
---
+++ init.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+echo 'Start init'
+
+if [ -z ${ROOT_PASSWORD} ]; then
+	echo "Use default password : Mjeedom96"
+	echo "root:Mjeedom96" | chpasswd
+else
+	echo "root:${ROOT_PASSWORD}" | chpasswd
+fi
+
+if [ -f /var/www/html/core/config/common.config.php ]; then
+	echo 'Jeedom is already install'
+else
+	echo 'Start jeedom installation'
+	rm -rf /root/install.sh
+	wget https://raw.githubusercontent.com/jeedom/core/beta/install/install.sh -O /root/install.sh
+	chmod +x /root/install.sh
+	echo "max_execution_time = 600" >> /usr/local/etc/php/conf.d/jeedom.ini
+	echo "upload_max_filesize = 1G" >> /usr/local/etc/php/conf.d/jeedom.ini
+	echo "post_max_size = 1G" >> /usr/local/etc/php/conf.d/jeedom.ini
+	echo "expose_php = Off" >> /usr/local/etc/php/conf.d/jeedom.ini
+	/root/install.sh -h 1 -s 0 > /var/www/html/index.html
+	/root/install.sh -h 1 -s 1 >> /var/www/html/index.html
+	/root/install.sh -h 1 -s 6 >> /var/www/html/index.html
+	/root/install.sh -h 1 -s 7 >> /var/www/html/index.html
+	/root/install.sh -h 1 -s 10 >> /var/www/html/index.html
+	/root/install.sh -h 1 -s 11 >> /var/www/html/index.html
+	rm -rf /var/www/html/index.html
+fi
+
+echo 'All init complete'
+chmod 777 /dev/tty*
+chmod 755 -R /var/www/html
+chown -R www-data:www-data /var/www/html
+
+echo 'Launch cron'
+service cron start
+
+echo 'Launch nginx'
+service nginx start
+
+/usr/bin/supervisord
